18e2bbb1dfa5b7f8c5eacf2890b5275225c32c50
hbahamonde <bahamondeh@gmail.com>
2017-09-06 11:24:15 -0500

    adds coordinates

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,14 +1,39 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA"
+    "New Orleans, USA",
+    29.9511,
+    90.0715
   ],
   [
-    "London, UK"
+    "New York, USA",
+    40.7128,
+    74.0059
   ],
   [
-    "San Francisco, California"
+    "Las Vegas, USA",
+    36.1699,
+    115.1398
   ],
   [
-    "Los Angeles, CA"
-  ]
+    "Philadelphia, USA",
+    39.9526,
+    75.1652
+  ],
+  [
+    "Chicago, USA",
+    41.8781,
+    87.6298
+  ],
+  [
+    "San Juan, Puerto Rico",
+    18.4655,
+    66.1057
+  ],
+  [
+    "Santiago, Chile",
+    33.4489,
+    70.6693
+  ],
+
+
 ];
