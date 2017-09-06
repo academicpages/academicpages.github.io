315b1ecbb9a2aabc3d516a42bb5552f25169a76e
hbahamonde <bahamondeh@gmail.com>
2017-09-06 11:16:26 -0500

    tests

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,14 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Berkeley CA, USA"
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "London, UK"
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "San Francisco, California"
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Los Angeles, CA"
   ]
 ];
