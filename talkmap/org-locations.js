0a6fb259965ec2730d4df98c277eb7f9af2490e6
Gracielle Higino <graciellehigino@hotmail.com>
2017-10-16 22:32:14 -0200

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,16 +1,16 @@
 var addressPoints = [
   [
-    "ICBS, UFAL, Maceió, Alagoas, Brazil",
+    "ICBS - UFAL, Maceió - Alagoas, Brazil",
     -9.558133,
     -35.775945
   ],
   [
-    "MHN, Maceió, Alagoas, Brazil",
+    "Museu de História Natural, Maceió - Alagoas, Brazil",
     -9.668918,
     -35.744810
   ]
   [
-    "Universidade Federal de Viçosa, Viçosa, Minas Gerais, Brazil",
+    "Universidade Federal de Viçosa, Viçosa - Minas Gerais, Brazil",
     -20.761235,
     -42.866402
   ]
