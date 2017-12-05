5dc2bcdb9d3e9d4beb4f4c4a32a3cfa64f4e5c25
Hector Bahamonde <hector.bahamonde@rutgers.edu>
2017-12-05 16:44:52 -0600

    improved map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,58 +1,58 @@
 var addressPoints = [
   // SPSA
   [
-    "New Orleans, USA", //SPSA 2015
-    29.9511,
-    -90.0715
+    "New Orleans, USA", //SPSA 2015 (Hyatt Regency)
+    29.949545,
+    -90.076742
   ],
   [
-    "New Orleans, USA", //SPSA 2018
-    29.9511,
-    -90.0715
+    "New Orleans, USA", //SPSA 2018 (Hyatt Regency)
+    29.949545,
+    -90.076742
   ],
   // LASA
   [
-    "Barcelona, Spain", // LASA 2018
+    "Barcelona, Spain", // LASA 2018 (Centre de Convencions Internacional de Barcelona)
     41.409584,
-    -2.219077
+    2.219077
   ],
   [
-    "New York, USA", // LASA 2016
+    "New York, USA", // LASA 2016 
     40.7128,
     -74.0059
   ],
    [
-  "Chicago, USA", // LASA: 2014
-    41.8781,
-    -87.6298
+  "Chicago, USA", // LASA: 2014 (Palmer House)
+    41.880646,
+    -87.627422
   ],
   // WPSA
     [
-    "San Francisco, USA", // WPSA 2018
-    37.773972,
-    -122.431297
+    "San Francisco, USA", // WPSA 2018 (HYATT REGENCY HOTEL IN EMBARCADERO)
+    37.794402,
+    -122.395844
   ],
   [
-    "Las Vegas, USA", // WPSA 2015
-    36.1699,
-    -115.1398
+    "Las Vegas, USA", // WPSA 2015 (Caesar Palace)
+    36.116243,
+    -115.174501
   ],
   // APSA
   [
-    "Philadelphia, USA", // APSA 2016
-    39.9526,
-    -75.1652
+    "Philadelphia, USA", // APSA 2016 (Pennsylvania Convention Center)
+    39.954973,
+    -75.159988
   ],
   // MPSA
   [
-    "Chicago, USA", // MPSA: 2017
-    41.8781,
-    -87.6298
+    "Chicago, USA", // MPSA: 2017 (Palmer House)
+    41.880646,
+    -87.627422
   ],
   [
-    "Chicago, USA", // MPSA: 2018
-    41.8781,
-    -87.6298
+    "Chicago, USA", // MPSA: 2018 (Palmer House)
+    41.880646,
+    -87.627422
   ],
   // Others
   [
