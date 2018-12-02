e28619b5ca13b4605b7b953c66a8600fc8fcddb0
Gracielle Higino <graciellehigino@hotmail.com>
2018-12-02 15:47:43 -0200

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,11 +1,11 @@
 var addressPoints = [
   [
-    "Universidade Federal de São João del-Rei, São João del-Rei - Minas Gerais, Brazil",
+    "São João del-Rei - Minas Gerais, Brazil",
     -21.123283,
     -44.250572
   ],
   [
-    "Universidade Federal de Goiás, Goiânia - Goiás, Brazil",
+    "Goiânia - Goiás, Brazil",
     -16.601620,
     -49.261391
   ],
@@ -18,7 +18,7 @@ var addressPoints = [
     "Museu de História Natural, Maceió - Alagoas, Brazil",
     -9.668918,
     -35.744810
-  ]
+  ],
   [
     "Universidade Federal de Viçosa, Viçosa - Minas Gerais, Brazil",
     -20.761235,
