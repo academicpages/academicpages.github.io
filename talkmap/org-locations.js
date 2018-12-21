af66b7a1248651da6f8bdc6d4e98c11d13abb0d4
julianeiler <45015758+julianeiler@users.noreply.github.com>
2018-12-21 13:14:43 -0800

    Update org-locations.js
    Added more locations

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -2,17 +2,22 @@ var addressPoints = [
   [
     "Las Vegas, Nevada",
     36.1699,
-    115.1398
+    -115.1398
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Berlin, Germany",
+    52.5200,
+    13.4050
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Munich, Germany",
+    48.1351,
+    11.5820
+  ],
+  [
+    "Rome, Italy",
+    41.9028,
+    12.4964
   ],
   [
     "Los Angeles, CA",
