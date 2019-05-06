8d7495c062ea758393c207cf059d127601b7d573
Nicholas Sale <nicholas.j.sale@gmail.com>
2019-05-06 14:28:09 +0100

    Create vallenato.js

---
+++ assets/vallenato/vallenato.js
@@ -0,0 +1,25 @@
+function vallenato() {
+	// Add "inactive" class to all headers.
+	$('.vallenato-header').toggleClass('vallenato-inactive');
+
+	// Add "closed" class to all content divs.
+	$('.vallenato-content').toggleClass('vallenato-closed');
+
+	// Open the first content section when the page loads.
+	$('.vallenato-header').first().toggleClass('vallenato-active vallenato-inactive');
+	$('.vallenato-content').first().toggleClass('vallenato-open vallenato-closed').slideDown();
+
+	// The Accordion Effect
+	$('.vallenato-header').click(function () {
+	    if($(this).is('.vallenato-inactive')) {
+	        $('.vallenato-active').toggleClass('vallenato-active vallenato-inactive').next().slideToggle().toggleClass('vallenato-open');
+	        $(this).toggleClass('vallenato-active vallenato-inactive');
+	        $(this).next().slideToggle().toggleClass('vallenato-open');
+	    }
+
+	    else {
+	        $(this).toggleClass('vallenato-active vallenato-inactive');
+	        $(this).next().slideToggle().toggleClass('vallenato-open');
+	    }
+	});
+}
