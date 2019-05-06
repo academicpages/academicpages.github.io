f361b904e8949db6d12100765105d515845d1163
Nicholas Sale <nicholas.j.sale@gmail.com>
2019-05-06 16:11:23 +0100

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,21 +1,21 @@
 var addressPoints = [
   [
-    "University of Strathclyde, Glasgow, UK\n<a href='http://events.cs.bham.ac.uk/syco/2/'>SYCO2</a>",
+    "University of Strathclyde, Glasgow, UK<br><a href='http://events.cs.bham.ac.uk/syco/2/'>SYCO2</a>",
     55.8612503,
     -4.2458069
   ],
   [
-    "New College, Oxford, UK",
+    "New College, Oxford, UK<br><a href='http://events.cs.bham.ac.uk/syco/3/'>SYCO3</a>",
     51.7549119,
     -1.2538205
   ],
   [
-    "Swansea University, UK",
+    "Swansea University, UK<br><a href='https://sites.google.com/view/dragon-applied-topology'>Dragon Applied Topology</a>",
     51.6186629,
     -3.8915309
   ],
   [
-    "ICTP, Trieste, Italy",
+    "ICTP, Trieste, Italy<br><a href='http://www.adaboralevi.com/TAGSS/2019.html'>TAGGS III</a>",
     45.7033941,
     13.7178389
   ]
