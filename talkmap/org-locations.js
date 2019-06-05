b953046004fc36ab81f17f4c61cc88bf5f45247a
Vítor V. Vasconcelos <vvvasconcelos@fc.ul.pt>
2019-06-05 14:51:36 -0400

    talkmap
    commit talkmaps

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,67 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Lisboa, Portugal",
+    38.7077507,
+    -9.1365919
+  ],
+  [
+    "Aveiro, Portugal",
+    40.640496,
+    -8.6537841
+  ],
+  [
+    "Sao Jose Campos, SP, Brasil",
+    -28.020043,
+    -49.470347
+  ],
+  [
+    "Barcelona, Italy",
+    45.1590586,
+    10.7952902
+  ],
+  [
+    "Taormina, Italy",
+    37.8517921,
+    15.2856523
+  ],
+  [
+    "Princeton, NJ, USA",
+    40.3492744,
+    -74.6592958
+  ],
+  [
+    "New York, NY, USA",
+    40.7127281,
+    -74.0060152
+  ],
+  [
+    "Rio de Mouro, Sintra, Portugal",
+    38.7723602,
+    -9.3256835
+  ],
+  [
+    "Oeiras, Portugal",
+    38.7124971,
+    -9.27130038240727
   ],
   [
     "London, UK",
     51.5073219,
-    -0.1276473
+    -0.1276474
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Boston, MA, USA",
+    42.3602534,
+    -71.0582912
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Potsdam, Germany",
+    52.4009309,
+    13.0591397
+  ],
+  [
+    "New Orleans, LA, USA",
+    29.9499323,
+    -90.0701156
   ]
 ];
