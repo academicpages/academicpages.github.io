7619812c8f2d45a2036e993d8a10716210c7b226
Emily Hastings <emhastings@knox.edu>
2019-07-08 23:44:09 -0400

    Last newline, talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,12 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Galesburg, IL",
+    40.9479779,
+    -90.3705842
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Jersey City, NJ",
+    40.7281575,
+    -74.0776417
   ]
 ];
