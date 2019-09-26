5b98740fbe6e4b3f9785c2d287046fae00ba2133
fjavifabre <fjavifabre@gmail.com>
2019-09-26 04:22:55 +0200

    Change master branch to new page with Hugo.

--- _projects/taranis.md
+++
@@ -1,9 +0,0 @@
----
-title: "Taranis"
-collection: projects
-excerpt: 'An OpenGL based render engine.'
-image: "taranis.png"
-authors: "F Javier Fabre, Borja Villarino"
-header:
-  teaser: "taranis.png"
----
---
+++ js/mathjax-config.js
@@ -0,0 +1,10 @@
+window.MathJax = {
+  CommonHTML: {linebreaks: {automatic: true}},
+  tex2jax: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['$$', '$$'], ['\\[', '\\]']],
+    processEscapes: false
+  },
+  TeX: {noUndefined: {attributes: {mathcolor: 'red', mathbackground: '#FFEEEE', mathsize: '90%'}}},
+  messageStyle: 'none'
+};
