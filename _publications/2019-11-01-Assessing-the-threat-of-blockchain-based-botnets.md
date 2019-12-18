8750fde08f3d410cd486e27effb5fa251598173c
Nikolaos Alexopoulos <alexopoulos@tk.tu-darmstadt.de>
2019-12-18 14:14:42 +0100

    added biblio

---
+++ _publications/2019-11-01-Assessing-the-threat-of-blockchain-based-botnets.md
@@ -0,0 +1,13 @@
+---
+title: "Assessing the threat of blockchain-based botnets"
+collection: publications
+permalink: /publication/2019-11-01-Assessing-the-threat-of-blockchain-based-botnets
+excerpt: '(to appear)'
+date: 2019-11-01
+venue: 'In the proceedings of eCrime 2019: Symposium on Electronic Crime Research'
+paperurl: 'http://tubiblio.ulb.tu-darmstadt.de/117622/'
+citation: ' Leon B{\&quot;o}ck,  Nikolaos Alexopoulos,  Emine Saracoglu,  Max M{\&quot;u}hlh{\&quot;a}user,  Emmanouil Vasilomanolakis, &quot;Assessing the threat of blockchain-based botnets.&quot; In the proceedings of eCrime 2019: Symposium on Electronic Crime Research, 2019.'
+---
+(to appear)
+
+[Access paper here](http://tubiblio.ulb.tu-darmstadt.de/117622/){:target="_blank"}
--- markdown_generator/pubsFromBib.py
+++ markdown_generator/pubsFromBib.py
@@ -27,20 +27,21 @@ import re
 #todo: incorporate different collection types rather than a catch all publications, requires other changes to template
 publist = {
     "proceeding": {
-        "file" : "proceedings.bib",
+        "file" : "publications.bib",
         "venuekey": "booktitle",
         "venue-pretext": "In the proceedings of ",
         "collection" : {"name":"publications",
                         "permalink":"/publication/"}
         
-    },
-    "journal":{
-        "file": "pubs.bib",
-        "venuekey" : "journal",
-        "venue-pretext" : "",
-        "collection" : {"name":"publications",
-                        "permalink":"/publication/"}
-    } 
+    }
+#    ,
+#    "journal":{
+#        "file": "pubs.bib",
+#        "venuekey" : "journal",
+#        "venue-pretext" : "",
+#        "collection" : {"name":"publications",
+#                        "permalink":"/publication/"}
+#    } 
 }
 
 html_escape_table = {
