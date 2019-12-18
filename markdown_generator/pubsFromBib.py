bcf48a50d4551b1e42877ef5ec6e16f0111d7a3e
Nikolaos Alexopoulos <alexopoulos@tk.tu-darmstadt.de>
2019-12-18 15:55:33 +0100

    small change in bib

--- markdown_generator/pubsFromBib.py
+++ markdown_generator/pubsFromBib.py
@@ -144,17 +144,11 @@ for pubsource in publist:
             if "dataset" in b.keys():
                 if len(str(b["dataset"])) > 5:
                     md += "\ndataset: '" + b["dataset"] + "'"
-                    url = True
+                    dataset = True
             
             ## Markdown description for individual page
 #            if note:
 #                md += "\n" + html_escape(b["note"]) + "\n"
-
-            if url:
-                md += "\n[Access paper here](" + b["url"] + "){:target=\"_blank\"}\n" 
-            
-            if dataset:
-                md += "\n[Access dataset here](" + b["dataset"] + "){:target=\"_blank\"}\n" 
 
             md_filename = os.path.basename(md_filename)
 
