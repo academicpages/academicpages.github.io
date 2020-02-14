f2a77f8fb981b76ac1062049ef0d285f5b952c61
unknown <exian@SHINECARBONX1.stanford.edu>
2020-02-13 23:50:40 -0800

    t

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,37 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
-  ],
-  [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
     "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    37.7790262,
+    -122.4199061
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Salt Lake City, Utah",
+    40.7596198,
+    -111.8867975
+  ],
+  [
+    "New Orleans, Louisiana",
+    29.9499323,
+    -90.0701156
+  ],
+  [
+    "Minneapolis, Minnesota",
+    44.9772995,
+    -93.2654692
+  ],
+  [
+    "Singapore, Singapore",
+    1.357107,
+    103.8194992
+  ],
+  [
+    "Los Angeles, California",
+    34.0536909,
+    -118.2427666
+  ],
+  [
+    "Boston, Massachusetts",
+    42.3602534,
+    -71.0582912
   ]
 ];
