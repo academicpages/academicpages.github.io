aa340f3f7ff83133cefe46b3aa778c4c8634a1f8
Yongjun Zhang <joshzyj@gmail.com>
2020-10-20 11:09:00 -0400

    Update Lab9.py

--- files/tutorial/Lab9.py
+++ files/tutorial/Lab9.py
@@ -22,11 +22,36 @@ os.chdir("/users/joshzyj/google drive/gdelt/")
 # You need to install google.cloud.bigquery first
 # pip install google.cloud.bigquery
 
+
+# Our task: query gdelt-bq database to extract protests
+# save extracted tables into our database GDELT under our own project.
+# We need to check the exisitence of gdelt database first and if not exists, we create a new one.
+
 from google.cloud import bigquery
+from google.cloud.exceptions import NotFound
 
 client = bigquery.Client()
+# TODO(developer): Set dataset_id to the ID of the dataset to determine existence.
+# dataset_id = "your-project.your_dataset"
+dataset_id = "integral-nimbus-160315.gdelt"
 
-#####################################
+try:
+    client.get_dataset(dataset_id)  # Make an API request.
+    print("Dataset {} already exists".format(dataset_id))
+except NotFound:
+    print("Dataset {} is not found".format(dataset_id))
+    print("Let us create {} for our project".format(dataset_id))
+    # Construct a full Dataset object to send to the API.
+    dataset = bigquery.Dataset(dataset_id)
+    # TODO(developer): Specify the geographic location where the dataset should reside.
+    dataset.location = "US"
+    # Send the dataset to the API for creation, with an explicit timeout.
+    # Raises google.api_core.exceptions.Conflict if the Dataset already
+    # exists within the project.
+    dataset = client.create_dataset(dataset, timeout=30)  # Make an API request.
+    print("Created dataset {}.{}".format(client.project, dataset.dataset_id))
+    
+# Then let us define the sql query and query the gdelt-bq database
 # The following codes query events_partitioned table and save a protest table
 # Note that we have not yet saved a table into local.
 # We remotely access bigquery and save a table into bigquery
@@ -75,6 +100,10 @@ query_job_1 = client.query(QUERY1,job_config=job_config_1)  # API request
 query_job_1.result()  # Waits for query to finish
 print("Query results loaded to the table {}".format(table_id_1))
 # So now we have a table sitting in our own big query. if you use your own browser to log into your google cloud, you should be able to see a table named protest under gdelt project...
+
+# You can check here for more sample codes:
+# https://cloud.google.com/bigquery/docs/samples
+# for instance, how to create a databaset, etc.
 
 ##########################
 
