cc2f5aae4ca3b4bd230df1a84bae793fd261e247
LeonardoPaccianiMori <leonardopaccianimori@gmail.com>
2021-01-27 12:36:58 +0100

    Map switched to OpenStreetmap

--- map/map.py
+++ map/map.py
@@ -47,9 +47,9 @@ map_preamble = """
     	<script>
             var mymap = L.map('mapid').setView([45,0], 2);
 
-            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {maxZoom: 18, attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 20, attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
                 '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                'Imagery © <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
                 id: 'mapbox.streets'}).addTo(mymap);
 
             var MarkerIcon = L.Icon.extend({
