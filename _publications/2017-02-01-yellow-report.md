169c51cc65bdcaa8190e8dbeb8f5a2654465a7f1
Rafael Teixeira de Lima <rafael.teixeira.de.lima@cern.ch>
2021-02-12 10:31:22 +0100

    publications

---
+++ _publications/2017-02-01-yellow-report.md
@@ -0,0 +1,8 @@
+---
+    type: "Yellow report"
+    title: "Handbook of LHC Higgs Cross Sections: 4. Deciphering the Nature of the Higgs Sector / Section: Prospects for prompt decays with MET: h to 2γ+MET test case."
+    authors: "T. Orimoto, R. Teixeira de Lima; D. de Florian, C. Grojean, F. Maltoni, C. Mariotti, A. Nikitenko, M. Pieri, P. Savard, M. Schumacher, R. Tanaka (editors)"
+    date: 2017-02-01
+    venue: "CERN Yellow Reports: Monographs"
+    paperurl: "https://cds.cern.ch/record/2227475"
+---
---
+++ ymls/generate_md.py
@@ -0,0 +1,25 @@
+import os, sys
+
+f = open('results.yml', 'r')
+name = ''
+lines = ''
+for l in f:
+    cs = l.split()
+
+    if '-' in cs[0]:
+
+        if name is not '':
+            fout = open('../_publications/'+name+'.md', 'w')
+            fout.write('---\n')
+            fout.write(lines)
+            fout.write('---\n')
+            fout.close()
+            lines = ''
+
+        name = l.replace('- ', '').replace(':', '').strip()
+    else:
+        lines += l
+
+    if 'date' in l:
+        name = l.strip().replace('date: ', '') + '-' + name
+        print name
