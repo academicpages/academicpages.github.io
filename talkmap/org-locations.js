10f6e8fe581157c8006b69d0d2223532edced108
Jordan Pierre <jordan.j.pierre@gmail.com>
2021-04-24 20:22:11 -0400

    Added new map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,212 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Medellin, Colombia",
+    6.2443382,
+    -75.573553
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Bogota, Colombia",
+    4.6533326,
+    -74.083652
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Santa Marta, Colombia",
+    11.2422289,
+    -74.2055606
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Amsterdam, Netherlands",
+    52.3727598,
+    4.8936041
+  ],
+  [
+    "Rotterdam, Netherlands",
+    51.9228934,
+    4.4631786
+  ],
+  [
+    "Leiden, Netherlands",
+    52.1518157,
+    4.4811088666204295
+  ],
+  [
+    "Antwerp, Belgium",
+    51.2211097,
+    4.3997081
+  ],
+  [
+    "Grand Cayman, Cayman Islands",
+    19.3298691,
+    -81.17122907994889
+  ],
+  [
+    "Little Cayman, Cayman Islands",
+    19.6853317,
+    -80.04777972988352
+  ],
+  [
+    "London Ontario, Canada",
+    42.9836747,
+    -81.2496068
+  ],
+  [
+    "Monteverde, Costa Rica",
+    10.30553,
+    -84.8096412
+  ],
+  [
+    "San Jose, Costa Rica",
+    9.9325427,
+    -84.0795782
+  ],
+  [
+    "Orlando, Florida, USA",
+    28.5421109,
+    -81.3790304
+  ],
+  [
+    "Tampa, Florida, USA",
+    27.9477595,
+    -82.458444
+  ],
+  [
+    "Miami, Florida, USA",
+    25.7741728,
+    -80.19362
+  ],
+  [
+    "Key West, Florida, USA",
+    24.5625566,
+    -81.7724368
+  ],
+  [
+    "Ft. Lauderdale, Florida, USA",
+    26.1223084,
+    -80.1433786
+  ],
+  [
+    "Columbus, Ohio, USA",
+    39.9622601,
+    -83.0007065
+  ],
+  [
+    "Cleveland, Ohio, USA",
+    41.5051613,
+    -81.6934446
+  ],
+  [
+    "Cincinnati, Ohio, USA",
+    39.1014537,
+    -84.5124602
+  ],
+  [
+    "Toledo, Ohio, USA",
+    41.6529143,
+    -83.5378173
+  ],
+  [
+    "Akron, Ohio, USA",
+    41.083064,
+    -81.518485
+  ],
+  [
+    "State College, Pennsylvania, USA",
+    40.7944504,
+    -77.8616386
+  ],
+  [
+    "Pittsburg, Pennsylvania, USA",
+    40.4416941,
+    -79.9900861
+  ],
+  [
+    "Mill Run, Pennsylvania, USA",
+    39.7630965,
+    -79.6659686
+  ],
+  [
+    "New York City, New York, USA",
+    40.7127281,
+    -74.0060152
+  ],
+  [
+    "Jackson Township, New Jersey, USA",
+    40.1151766,
+    -74.3647772
+  ],
+  [
+    "Atlanta, Georgia, USA",
+    33.7489924,
+    -84.3902644
+  ],
+  [
+    "Washington DC, USA",
+    38.8950368,
+    -77.0365427
+  ],
+  [
+    "Chicago, Illinois, USA",
+    41.8755616,
+    -87.6244212
+  ],
+  [
+    "Pecatonica, Illinois, USA",
+    42.3138917,
+    -89.3593778
+  ],
+  [
+    "Gatlinburg, Tennessee, USA",
+    35.714259,
+    -83.5101638
+  ],
+  [
+    "Pigeon Forge, Tennessee, USA",
+    35.7884241,
+    -83.5543329
+  ],
+  [
+    "Erwin, Tennessee, USA",
+    36.1451082,
+    -82.4168055
+  ],
+  [
+    "Richmond, Virginia, USA",
+    37.5385087,
+    -77.43428
+  ],
+  [
+    "Shenandoah, Virginia, USA",
+    38.4851245,
+    -78.625016
+  ],
+  [
+    "Detroit, Michigan, USA",
+    42.3315509,
+    -83.0466403
+  ],
+  [
+    "Louisville, Kentucky, USA",
+    38.2542376,
+    -85.759407
+  ],
+  [
+    "Corbin, Kentucky, USA",
+    36.9486986,
+    -84.0968761
+  ],
+  [
+    "Mammoth Cave, Kentucky, USA",
+    37.1861597,
+    -86.0999753
+  ],
+  [
+    "Circleville, West Virginia, USA",
+    38.6709481,
+    -79.4911597
+  ],
+  [
+    "Duck, North Carolina, USA",
+    36.169608,
+    -75.7551854
   ]
 ];
