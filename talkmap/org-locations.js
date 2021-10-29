81b6865aee7808a466389c538ecfd44b179f6103
rtalaverag <rtalaveragarcia@ucm.es>
2021-10-29 14:02:41 +0200

    talkmap udate

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -3,5 +3,25 @@ var addressPoints = [
     "Madrid, Spain",
     40.4167047,
     -3.7035825
+  ],
+  [
+    "Madrid, Espa\u00f1a",
+    40.4167047,
+    -3.7035825
+  ],
+  [
+    "Dublin, Ireland",
+    53.3497645,
+    -6.2602732
+  ],
+  [
+    "Granada, Espa\u00f1a",
+    37.1734995,
+    -3.5995337
+  ],
+  [
+    "Santiago de Chile, Chile",
+    -33.4377756,
+    -70.6504502
   ]
 ];
