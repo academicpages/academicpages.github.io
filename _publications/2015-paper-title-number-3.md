774594084129126592409acefd5c21aee7d31785
abehofman <hofman.abe@gmail.com>
2022-02-17 14:42:43 +0100

    play with pubs

--- _publications/2015-paper-title-number-3.md
+++ _publications/2015-paper-title-number-3.md
@@ -1,9 +1,9 @@
 ---
-title: "Paper Title Number 3"
+title: "Paper Title Number 10"
 collection: publications
-permalink: /publication/2015-10-01-paper-title-number-3
+permalink: /publication/01/10/2015-paper-title-number-3
 excerpt: 'This paper is about the number 3. The number 4 is left for future work.'
-date: 2015-10-01
+date: 01/10/2015
 venue: 'Journal 1'
 paperurl: 'http://academicpages.github.io/files/paper3.pdf'
 citation: 'Your Name, You. (2015). &quot;Paper Title Number 3.&quot; <i>Journal 1</i>. 1(3).'
--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -24,7 +24,7 @@
 # In[2]:
 
 import pandas as pd
-
+import os
 
 # ## Import TSV
 # 
@@ -33,6 +33,8 @@ import pandas as pd
 # I found it important to put this data in a tab-separated values format, because there are a lot of commas in this kind of data and comma-separated values can get messed up. However, you can modify the import statement, as pandas also has read_excel(), read_json(), and others.
 
 # In[3]:
+
+os.chdir("/Users/abehofman/Documents/GitHub/abehofman.github.io/markdown_generator")
 
 publications = pd.read_csv("publications.tsv", sep="\t", header=0)
 publications
