fb6c3e38e86d5c47d8b5c45153407247e48e1d0b
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-06-22 13:52:38 -0500

    Update publish script

--- publish_brain.sh
+++ publish_brain.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-/bin/python3 -m jupyter nbconvert --to html --execute publish_brain.ipynb --output logs/publish_brain_$(date +"%Y%m%d%H%M").html
+outfile="logs/publish_brain_$(date +"%Y%m%d%H%M").html"
+/bin/python3 -m jupyter nbconvert --to html --execute publish_brain.ipynb --output $outfile
+firefox $outfile
