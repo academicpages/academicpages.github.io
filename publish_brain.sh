a7f68f15a92dcc8b2c5171dc94345ab8c1643418
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-06-23 09:40:50 -0500

    QOL - Remove .md from autofill title - Check file exists on check new, avoiding error - Only open nbconvert report if no error

--- publish_brain.sh
+++ publish_brain.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 outfile="logs/publish_brain_$(date +"%Y%m%d%H%M").html"
-/bin/python3 -m jupyter nbconvert --to html --execute publish_brain.ipynb --output $outfile
-firefox $outfile
+/bin/python3 -m jupyter nbconvert --to html --execute publish_brain.ipynb --output $outfile \
+    && firefox $outfile
