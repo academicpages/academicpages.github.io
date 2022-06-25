27224e4f2de007e8b3408796b494c8cd35dc5169
sunnyCST <805924646@qq.com>
2022-06-25 12:19:21 +0800

    remove demo paper

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -102,7 +102,7 @@ for row, item in publications.iterrows():
     
     md_filename = os.path.basename(md_filename)
        
-    with open("../_publications/" + md_filename, 'w') as f:
-        f.write(md)
+    # with open("../_publications/" + md_filename, 'w') as f:
+    #     f.write(md)
 
 
