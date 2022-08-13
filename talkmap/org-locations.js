ccd50f6a29fe8375ffd6bd471fa43d964c0200c9
Keming Zhang <zhangkm@sjtu.edu.cn>
2022-08-14 00:22:40 +0800

    map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,137 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Valencia, Spain. November 2016. Congress of online political communication. Mediaflows.", 
+    39.4699014, 
+    -0.3759513
+  ], 
+  [
+    "Budapest, Hungary. December 2012. The EU and the Politicization of Europe. Euroacademia.", 
+    47.4983815, 
+    19.0404707
+  ], 
+  [
+    "Lisbon, Portugal. June 2014. European Dialogue on Internet Governance. EuroDIG.", 
+    38.7077926, 
+    -9.1365061
+  ], 
+  [
+    "Tampere, Finland. August 2017. NordMedia 2017 bi-annual Conference. Nordicom.", 
+    61.4980214, 
+    23.7603118
+  ], 
+  [
+    "Berlin, Germany. September 2012. Europe’s common future. Böll Foundation.", 
+    52.5170365, 
+    13.3888599
+  ], 
+  [
+    "Milan, Italy. June 2017. 7th Conference European Political Sciences. EPSA.", 
+    45.4667971, 
+    9.1904984
+  ], 
+  [
+    "Vilnius, Lithuania. December 2013. Closing Conference of the Lithuanian Presidency. Council of the EU.", 
+    54.6870458, 
+    25.2829111
+  ], 
+  [
+    "Loughborough, UK. September 2013. 14th Annual UACES Forum Conference. UACES.", 
+    52.7723859, 
+    -1.2077985
+  ], 
+  [
+    "Cologne, Germany. July 2017. 3rd Computational Social Science Conference. IC2S2.", 
+    50.938361, 
+    6.959974
+  ], 
+  [
+    "Copenhagen, Denmark. June 2012. Conference United We Stand. Copenhagen Business School CBS.", 
+    55.6867243, 
+    12.5700724
+  ], 
+  [
+    "Istanbul, Turkey. May 2011. Accession of Turkey to the EU: Expectations and Realities. Bogazici University.", 
+    41.0096334, 
+    28.9651646
+  ], 
+  [
+    "Malm\u00f6, Sweden. April 2012. International Seminar Our Europe 2021. Europaforum.", 
+    55.6052931, 
+    13.0001566
+  ], 
+  [
+    "London, UK. June 2016. PhD Symposium Media and Communications. London Shool of Economics.", 
+    51.5073219, 
+    -0.1276474
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Milan, Italy. November 2016. Workshop at Public Opinion and Media Lab. University of Milan.", 
+    45.465428, 
+    9.199076
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Berlin, Germany. June 2013. European Dialogue on Internet Governance. EuroDIG.", 
+    52.526614, 
+    13.385468
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
-  ]
+    "Oslo, Norway. September 2017. ECPR General Conference.", 
+    59.5139, 
+    10.7522  
+  ], 
+  [
+    "Stockholm, Sweden. June 2012. European Dialogue on Internet Governance. EuroDIG.", 
+    59.329323, 
+    18.068581
+  ],
+  [
+    "Tartu, Estonia. October 2017. Association of Internet Researchers. AOIR.", 
+    58.377, 
+    26.72900 
+  ],
+  [
+    "Stockholm, Sweden. November 2017. ECREA Symposium Digital Democracy in the age of Big Data. Södertorns Högskola", 
+    59.220452, 
+    17.943596 
+  ],
+  [
+    "Hamburg, Germany. August 2018. ECPR General Conference", 
+    53.551086, 
+    9.9936
+  ], 
+  [
+    "Lugano, Switzerland. November 2018. ECREA Bi-anual General Conference", 
+    46.0036, 
+    8.9510
+  ],
+  [
+    "Sundsvall, Sweden. April 2018. Seminar at Mid Sweden University", 
+    62.39081, 
+    17.30692
+  ],
+  [
+    "Florence, Italy. February 2018. Seminar at Scuola Normale Superiore", 
+    43.768700, 
+    11.256930
+  ],
+  [
+    "Brussels, Belgium. March 2019. Seminar at European Commission DG COMM", 
+    50.8503, 
+    4.35100
+  ],
+  [
+    "Madrid, Spain. January 2019. OpenEUdebate.eu Opening Conference", 
+    40.4167, 
+    -3.703
+  ],
+  [
+    "Hildesheim, Germany. May 2019. Conference Transformation of the Public Sphere", 
+    51.1509, 
+    9.95100
+  ],
+  [
+    "Vancouver, Canada. May 2020. Misinformation in Canada, SFU Centre for Dialogue", 
+    49.2827, 
+    -123.1207
+  ],        
 ];
