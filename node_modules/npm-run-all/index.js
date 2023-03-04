8ceee2aaed4f279882b30e795b8c7d7cb2514537
Anne Drew Hu <48420747+hu-a@users.noreply.github.com>
2023-03-04 14:24:13 -0500

    Remove node_modules

--- node_modules/npm-run-all/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * @author Toru Nagashima
- * @copyright 2015 Toru Nagashima. All rights reserved.
- * See LICENSE file in root directory for full license.
- */
-"use strict";
-
-module.exports = require("./lib/npm-run-all").default;
