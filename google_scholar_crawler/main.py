d50e79209e8aa756bce46c66b8cbb76c27e29e08
Liu Yunqing <48918246+LiuYunqing@users.noreply.github.com>
2023-05-02 12:10:54 +0800

    Create main.py

---
+++ google_scholar_crawler/main.py
@@ -0,0 +1,23 @@
+from scholarly import scholarly
+import jsonpickle
+import json
+from datetime import datetime
+import os
+
+author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
+scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+name = author['name']
+author['updated'] = str(datetime.now())
+author['publications'] = {v['author_pub_id']:v for v in author['publications']}
+print(json.dumps(author, indent=2))
+os.makedirs('results', exist_ok=True)
+with open(f'results/gs_data.json', 'w') as outfile:
+    json.dump(author, outfile, ensure_ascii=False)
+
+shieldio_data = {
+  "schemaVersion": 1,
+  "label": "citations",
+  "message": f"{author['citedby']}",
+}
+with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
+    json.dump(shieldio_data, outfile, ensure_ascii=False)
