b77a81aa874d65d9f5c54c887cb0a0801f123fd9
taoanhuang <81658792+taoanhuang@users.noreply.github.com>
2023-05-30 13:18:21 -0700

    Add files via upload

---
+++ static/js/index.js
@@ -0,0 +1,21 @@
+window.HELP_IMPROVE_VIDEOJS = false;
+
+
+$(document).ready(function() {
+    // Check for click events on the navbar burger icon
+
+    var options = {
+			slidesToScroll: 1,
+			slidesToShow: 1,
+			loop: true,
+			infinite: true,
+			autoplay: true,
+			autoplaySpeed: 5000,
+    }
+
+		// Initialize all div with carousel class
+    var carousels = bulmaCarousel.attach('.carousel', options);
+	
+    bulmaSlider.attach();
+
+})
