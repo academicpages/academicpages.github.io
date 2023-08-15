c7e421608c22b301cc8fff4225b191bd41084330
Yunqing Liu <48918246+LiuYunqing@users.noreply.github.com>
2023-08-15 16:40:38 +0800

    Update voxel-dog.js

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -12,7 +12,7 @@ const VoxelDog = () => {
   const refContainer = useRef()
   const [loading, setLoading] = useState(true)
   const refRenderer = useRef()
-  const urlDogGLB = (process.env.NODE_ENV === 'production' ? 'https://craftzdog.global.ssl.fastly.net/homepage' : '') + '/dog.glb'
+  const urlDogGLB = './sazabi_msn-04_gundam.glb'
 
   const handleWindowResize = useCallback(() => {
     const { current: renderer } = refRenderer
