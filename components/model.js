64b0dda0f5c53e3cff7eedec248314f8f4e48638
Yunqing Liu <48918246+LiuYunqing@users.noreply.github.com>
2023-08-15 16:35:45 +0800

    Create model.js

---
+++ components/model.js
@@ -0,0 +1,37 @@
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+export function loadGLTFModel(
+  scene,
+  glbPath,
+  options = { receiveShadow: true, castShadow: true }
+) {
+  const { receiveShadow, castShadow } = options
+  return new Promise((resolve, reject) => {
+    const loader = new GLTFLoader()
+
+    loader.load(
+      glbPath,
+      gltf => {
+        const obj = gltf.scene
+        obj.name = 'dog'
+        obj.position.y = 0
+        obj.position.x = 0
+        obj.receiveShadow = receiveShadow
+        obj.castShadow = castShadow
+        scene.add(obj)
+
+        obj.traverse(function (child) {
+          if (child.isMesh) {
+            child.castShadow = castShadow
+            child.receiveShadow = receiveShadow
+          }
+        })
+        resolve(obj)
+      },
+      undefined,
+      function (error) {
+        reject(error)
+      }
+    )
+  })
+}
