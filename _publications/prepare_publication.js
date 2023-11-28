5d8708870d892bb50ab4a4601e1b3d54391023c6
WElab <welab@email.swu.edu.cn>
2023-11-29 02:14:32 +0800

    1

--- _publications/prepare_publication.js
+++ _publications/prepare_publication.js
@@ -3,16 +3,15 @@ async function fetchAndParseBibtex(url) {
         const response = await fetch(url);
         const bibtexText = await response.text();
 
-        let parsedData = parseBibtex(bibtexText); // 假设 parseBibtex 是你的解析函数
+        let parsedData = parseBibtex(bibtexText); 
+
+        return parsedData
 
         console.log(parsedData);
     } catch (error) {
         console.error("Error fetching or parsing BibTeX file:", error);
     }
 }
-
-fetchAndParseBibtex('./2023-paper.bib');
-
   
 function parseBibtex(bibtex) {
     let entries = [];
@@ -26,11 +25,10 @@ function parseBibtex(bibtex) {
                 entries.push(currentEntry);
                 currentEntry = {};
             }
-            // 获取条目类型
             currentEntry.type = line.match(/@(\w+)/)[1];
         } else if (line.includes('=')) {
             let [key, value] = line.split('=').map(s => s.trim());
-            value = value.replace(/[{}]/g, ''); // 移除花括号
+            value = value.replace(/[{}]/g, ''); 
             currentEntry[key] = value;
         }
     });
@@ -41,8 +39,35 @@ function parseBibtex(bibtex) {
 
     return entries;
 }
-  
-  // 转换为JSON
-  let jsonResult = parseBibtex(bibtexString);
-  console.log(jsonResult);
-  
+
+function generateArticleHtml(articles) {
+
+    html += '<ul>';
+    articles.forEach(article => {
+        let authorsArray = article.authors.split("and ");
+        let authorHtml = "";
+        authorsArray.forEach((author, index) => {
+            author = author.trim();
+            if (author === "Wei, Dongtao") {
+                if (article.mark) { // first author
+                    authorHtml += `<strong>${author}</strong><sup>${article.mark}</sup>, `;
+                } else { // 
+                    authorHtml += `<strong>${author}</strong>`;
+                }
+            } else {
+                authorHtml += `${author}`;
+            }
+        });
+        authorHtml = authorHtml.trim().replace(/,\s*$/, "");
+
+        html += `<li><a class='article-link' target='_blank' href='https://doi.org/${article.doi}'>${article.title}</a><a class='pdf-link' target='_blank' href="${article.pdflink}">[PDF]</a></li>`;
+        if (article.if) {
+            html += `<p>${article.authors}. (${article.year}). <i>${article.journal}</i>>(<strong>IF=${article.if}</strong>).</p>`;
+        } else {
+            html += `<p>${article.authors}. (${article.year}). <i>${article.journal}</i>.</p>`;
+        }
+        
+    });
+    html += '</ul>';
+    return html
+}
