b0b6ebe3ae3fc7f15fcb811668c1594a48a38644
SatyajithBoyana <Satyajith92@gmail.com>
2024-01-23 21:00:08 -0500

    disable enable submit based on captcha

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -94,5 +94,5 @@ $(document).ready(function(){
     closeOnContentClick: true,
     midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
   });
-
+  
 });
