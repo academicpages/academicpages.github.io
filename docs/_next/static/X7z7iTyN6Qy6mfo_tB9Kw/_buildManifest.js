e22c241796ed12ae230368fa735b996102d15f9a
edotau <ericdotau@gmail.com>
2024-01-24 21:44:08 -0800

    fix: css

--- docs/_next/static/X7z7iTyN6Qy6mfo_tB9Kw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/445c4173ffc2bf04.css","static/chunks/377-3e5bf92f2a0efe6b.js","static/chunks/pages/index-7d47a7e5e5081abf.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
