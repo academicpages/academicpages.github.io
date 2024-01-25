8198256868b1b3d4ce197feabbda8ed6cf88ba3d
edotau <ericdotau@gmail.com>
2024-01-24 20:37:39 -0800

    feat: nextera docs

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
 import { Provider as StyletronProvider } from "styletron-react";
 import { styletron } from "../styletron";
 
+import { ThemeProvider } from "atomize";
+
+const theme = {
+  colors: {
+    black900: "#1d1d1e",
+    greyLight: "#f2f3f4",
+    border: "#d1d1d1"
+  },
+  rounded: {
+    br1: "4px"
+  }
+};
+
 export default function App({ Component, pageProps }) {
   return (
     <StyletronProvider value={styletron}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </StyletronProvider>
   );
 }
