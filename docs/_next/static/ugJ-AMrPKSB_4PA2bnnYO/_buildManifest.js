cb530d4ae554fa7513345acd6e87d12ba49db08e
edotau <ericdotau@gmail.com>
2024-01-24 20:25:06 -0800

    nextera docs

--- docs/_next/static/ugJ-AMrPKSB_4PA2bnnYO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-ad8fd4851c40e56d.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
