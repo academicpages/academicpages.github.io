7f2309a9628bbac627fabac9677d5249c0c23bcd
Bhabesh Mali <56470824+BhabeshMali@users.noreply.github.com>
2024-02-05 15:22:50 +0530

    Update

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -34,7 +34,7 @@ import pandas as pd
 
 # In[3]:
 
-publications = pd.read_csv("publications_1.tsv", sep="\t", header=0)
+publications = pd.read_csv("publications.tsv", sep="\t", header=0)
 publications
 
 
