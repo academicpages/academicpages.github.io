5301355202554e3a3d3099bf2f24ce300aa611f0
Anna L.D. Latour <anna@fake.com>
2024-06-16 12:32:29 +0200

    Adding more talks.

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -8,5 +8,15 @@ var addressPoints = [
     "Arizona State University, School of Computing and Augmented Intelligence: Tempe, AZ, USA",
     33.423595929604254,
     -111.93974132380079
+  ],
+  [
+    "MCW 2020 at SAT 2020, in Alghero, Italy (online talk)",
+    40.5583503811401,
+    8.319171269390926
+  ],
+  [
+    "StarAI @ IJCAI 2018, Stockholmsmässan, The Stockholm Convention Center, Stockholm, Sweden",
+    59.277384836892494,
+    18.015202487954067
   ]
 ];
