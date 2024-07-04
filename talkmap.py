648fbf4daadffb33afecf367ec8c501c99dce3f1
tiexingwang <tiexingwang1992@gmail.com>
2024-07-04 15:12:16 +0100

    Update talkmap.py

--- talkmap.py
+++ talkmap.py
@@ -15,32 +15,31 @@ import glob
 import getorg
 from geopy import Nominatim
 
+# Set a custom user agent
+geocoder = Nominatim(user_agent="personal_website_tiexingwang")
+
 g = glob.glob("*.md")
 
-
-geocoder = Nominatim()
 location_dict = {}
 location = ""
 permalink = ""
 title = ""
 
-
 for file in g:
     with open(file, 'r') as f:
         lines = f.read()
-        if lines.find('location: "') > 1:
+        if 'location: "' in lines:
             loc_start = lines.find('location: "') + 11
             lines_trim = lines[loc_start:]
             loc_end = lines_trim.find('"')
             location = lines_trim[:loc_end]
-                            
-           
-        location_dict[location] = geocoder.geocode(location)
-        print(location, "\n", location_dict[location])
-
+            
+            location_dict[location] = geocoder.geocode(location)
+            print(location, "\n", location_dict[location])
 
 m = getorg.orgmap.create_map_obj()
 getorg.orgmap.output_html_cluster_map(location_dict, folder_name="../talkmap", hashed_usernames=False)
+
 
 
 
