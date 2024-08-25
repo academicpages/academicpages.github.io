e8c719ed771d27e7bf551095e1ab8513973c68c7
Angadh Nanjangud <angadh.n@gmail.com>
2024-08-25 16:47:08 +0100

    Site updated at 2024-08-25 15:47:08 UTC

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,84 +1,49 @@
----
-layout: none
----
-
-var store = [
-  {%- for c in site.collections -%}
-    {%- if forloop.last -%}
-      {%- assign l = true -%}
-    {%- endif -%}
-    {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%}
-    {%- for doc in docs -%}
-      {%- if doc.header.teaser -%}
-        {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture -%}
-      {%- else -%}
-        {%- assign teaser = site.teaser -%}
-      {%- endif -%}
-      {
-        "title": {{ doc.title | jsonify }},
-        "excerpt":
-          {%- if site.search_full_content == true -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | jsonify }},
-          {%- else -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | truncatewords: 50 | jsonify }},
-          {%- endif -%}
-        "categories": {{ doc.categories | jsonify }},
-        "tags": {{ doc.tags | jsonify }},
-        "url": {{ doc.url | relative_url | jsonify }},
-        "teaser": {{ teaser | relative_url | jsonify }}
-      }{%- unless forloop.last and l -%},{%- endunless -%}
-    {%- endfor -%}
-  {%- endfor -%}{%- if site.lunr.search_within_pages -%},
-  {%- assign pages = site.pages | where_exp:'doc','doc.search != false' -%}
-  {%- for doc in pages -%}
-    {%- if forloop.last -%}
-      {%- assign l = true -%}
-    {%- endif -%}
-  {
-    "title": {{ doc.title | jsonify }},
-    "excerpt":
-        {%- if site.search_full_content == true -%}
-          {{ doc.content | newline_to_br |
-            replace:"<br />", " " |
-            replace:"</p>", " " |
-            replace:"</h1>", " " |
-            replace:"</h2>", " " |
-            replace:"</h3>", " " |
-            replace:"</h4>", " " |
-            replace:"</h5>", " " |
-            replace:"</h6>", " "|
-          strip_html | strip_newlines | jsonify }},
-        {%- else -%}
-          {{ doc.content | newline_to_br |
-            replace:"<br />", " " |
-            replace:"</p>", " " |
-            replace:"</h1>", " " |
-            replace:"</h2>", " " |
-            replace:"</h3>", " " |
-            replace:"</h4>", " " |
-            replace:"</h5>", " " |
-            replace:"</h6>", " "|
-          strip_html | strip_newlines | truncatewords: 50 | jsonify }},
-        {%- endif -%}
-      "url": {{ doc.url | absolute_url | jsonify }}
-  }{%- unless forloop.last and l -%},{%- endunless -%}
-  {%- endfor -%}
-{%- endif -%}]
+var store = [{
+        "title": "Three major issues in today's consumer Internet",
+        "excerpt":"Much of today’s `free’ or consumer internet is built on three principles: Involuntary user data exploitation: Recent debates around generative AI have returned attention to this. Novel manipulation of users by malicious actors through personal chat: WhatsApp is used as a political weapon to broadcast misinformation during elections, especially in...","categories": ["Social Networking","Internet"],
+        "tags": [],
+        "url": "/social%20networking/internet/Improve-Internet/",
+        "teaser": null
+      },{
+        "title": "Life in Contempary Academia",
+        "excerpt":"Autonomy  is in short supply because it is not trait that is in demand.   ","categories": ["Habits"],
+        "tags": [],
+        "url": "/habits/Frustrations/",
+        "teaser": null
+      },{
+        "title": "Lagrange's equations for rocket-type variable mass systems",
+        "excerpt":"This paper examines the geometry of motion and nutation stability in free axisymmetric variable mass systems.   Download paper here   Recommended citation: Nanjangud, A. and Eke, F. O. (2018). “Lagrange’s equations for rocket-type variable mass systems.” International Review of Aerospace Engineering. 5(5), 255-260.  ","categories": ["manuscripts"],
+        "tags": [],
+        "url": "/publication/2012-10-01-lagrange-equations-rocket-type",
+        "teaser": null
+      },{
+        "title": "Approximate solution to the angular speeds of a nearly-symmetric mass-varying cylindrical body",
+        "excerpt":"This paper presents an approximate solution to the angular speeds of a nearly-symmetric mass-varying cylindrical body.   Download paper here   Recommended citation: Nanjangud, A. and Eke, F. O. (2017). “Approximate solution to the angular speeds of a nearly-symmetric mass-varying cylindrical body.” The Journal of the Astronautical Sciences. 64(2), 99-117.  ","categories": ["conferences"],
+        "tags": [],
+        "url": "/publication/2017-06-01-approximate-solution-angular-speeds",
+        "teaser": null
+      },{
+        "title": "Robotics and AI-enabled on-orbit operations with future generation of small satellites",
+        "excerpt":"This paper discusses robotics and AI-enabled on-orbit operations with future generation of small satellites.   Download paper here   Recommended citation: Nanjangud, A., Blacker, P. C., Bandyopadhyay, S., and Gao, Y. (2018). “Robotics and AI-enabled on-orbit operations with future generation of small satellites.” Proceedings of the IEEE. 106(3), 429-439.  ","categories": ["manuscripts"],
+        "tags": [],
+        "url": "/publication/2018-03-01-robotics-ai-enabled-on-orbit-operations",
+        "teaser": null
+      },{
+        "title": "Geometry of motion and nutation stability of free axisymmetric variable mass systems",
+        "excerpt":"This paper examines the geometry of motion and nutation stability in free axisymmetric variable mass systems.   Download paper here   Recommended citation: Nanjangud, A. (2018). “Geometry of motion and nutation stability of free axisymmetric variable mass systems.” Nonlinear Dynamics. 94(3), 2205-2218.  ","categories": [],
+        "tags": [],
+        "url": "/publication/2018-05-01-geometry-of-motion-nutation-stability",
+        "teaser": null
+      },{
+        "title": "Angular momentum of free variable mass systems is partially conserved",
+        "excerpt":"This paper demonstrates that angular momentum of free variable mass systems is partially conserved.   Download paper here   Recommended citation: Nanjangud, A. and Eke, F. O. (2018). “Angular momentum of free variable mass systems is partially conserved.” Aerospace Science and Technology. 79, 1-4.  ","categories": [],
+        "tags": [],
+        "url": "/publication/2018-08-01-angular-momentum-free-variable-mass-systems",
+        "teaser": null
+      },{
+        "title": "Towards Robotic On-Orbit Assembly of Large Space Telescopes: Mission Architectures, Concepts, and Analyses",
+        "excerpt":"This paper discusses mission architectures and concepts for robotic on-orbit assembly of large space telescopes. Download paper here Recommended citation: Nanjangud, A., Underwood, C., Rai, C. M., Eckersley, S., Sweeting, M., and Bianco. P. (2023). “Towards Robotic On-Orbit Assembly of Large Space Telescopes: Mission Architectures, Concepts, and Analyses.” Acta Astronautica....","categories": ["manuscripts"],
+        "tags": [],
+        "url": "/publication/2023-06-01-towards-robotic-on-orbit-assembly",
+        "teaser": null
+      }]
