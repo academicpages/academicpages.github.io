abf79c4c47db7335ddc6a9f7d7bdfdb834dc29d7
Changsoo Kim <yustchang@gmail.com>
2024-11-22 13:27:20 +0900

    Update publications.py

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -93,7 +93,7 @@ for row, item in publications.iterrows():
     ## Markdown description for individual page
     
     if len(str(item.paper_url)) > 5:
-        md += "\n\n<a href='" + item.paper_url + "'>Download paper here</a>\n" 
+        md += "\n\n<a href='" + item.paper_url + "'>Paper link</a>\n" 
         
     if len(str(item.excerpt)) > 5:
         md += "\n" + html_escape(item.excerpt) + "\n"
