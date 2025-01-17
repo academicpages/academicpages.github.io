b4d458503c6be92292ad62f0b6089f3bfa7fd21e
xianshine <xiankong@stanford.edu>
2025-01-17 22:29:22 +0800

    updated talks

--- sync_git.sh
+++ sync_git.sh
@@ -1,4 +1,6 @@
 
+# To push to github. Just run: ./sync_git.sh "changes made"
+
 #find ./ -type f -exec ls -lt {} + | head -n 10
 
 #git add -A
@@ -8,3 +10,4 @@
 git commit -a -m "$@"
 git push
 
+# To update talkmap. Run "python ../talkmap.py" in the folder of _talks/
